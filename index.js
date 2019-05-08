6229f048379b1fea2bcf9d918ece0d110d9c09c0
PravallikaKarella <geethikakarella@gmail.com>
2019-05-08 07:48:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import EmployeeList from './components/employeelist';
+import EmployeeList from './employeelist';
 
 ReactDOM.render(<div>EmployeeList
        <EmployeeList></EmployeeList></div>,document.getElementById("root"));
